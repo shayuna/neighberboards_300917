1fea2bf12498534dc7ef1583b677754ebe71b372
Shay Zaltsman <Shay@native.local>
2017-10-14 11:20:45 +0200

    mlab real uri plus res.send in insertdata function

--- routes/main.js
+++ routes/main.js
@@ -1,8 +1,8 @@
 var express=require("express");
 var router=express.Router();
 var mongoClient=require("mongodb").MongoClient;
-//var url="mongodb://shark:dbmonro@ds119355.mlab.com:19355/globe";
-var url="mongodb://localhost:27017/globe";
+var url="mongodb://shark:dbmonro@ds119355.mlab.com:19355/globe";
+//var url="mongodb://localhost:27017/globe";
 
 
 router.get("/",function(req,res,next){
@@ -48,7 +48,8 @@ router.post("/insertData",function(req,res,next){
             db.collection("neighberhoods").insert({location:{type:"Point",coordinates:[parseFloat(req.body.longitude),parseFloat(req.body.latitude)]},info:req.body.info},function(err,result){
                 if (!err){
                     db.collection("neighberhoods").createIndex({location:"2dsphere"});
-                    console.log ("record inserted successfully");
+                    console.log ("record inserted successfully hooray");
+                    res.send("we did it. without you it wasn't possible");
                 }
                 else{
                     console.log ("error when inserting record")
